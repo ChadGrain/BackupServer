74401896293a79a02d1b4cef5cc05c2fc5cd6d7b
ChadGrain <austingraingerwork@gmail.com>
2018-08-21 14:08:22 -0400

    Added Role Model and updated CreateUser and Update User

---
+++ ChurchServer/ChurchServer/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchServer.Models
+{
+    public class Role
+    {
+        public int RoleID;
+        public string RoleName;
+        public string RoleDesc;
+    }
+}
